fabba4d0e407028701a72165331d3240276286ca
Yoav Moran <yoav.m@taboola.com>
2017-01-02 08:17:27 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to log file');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// })
+
+app.get('/', (req, res) => {
+  res.send('Hello express');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'BAD_URL'
+  });
+});
+
+app.listen(3000);

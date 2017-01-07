6a1ec03e352611a237e23b1e8093030b1237a57a
Yoav Moran <yoav.m@taboola.com>
2017-01-07 21:50:07 +0200

    workinh on lea70 site

---
+++ public/js/gallery.js
@@ -0,0 +1,39 @@
+var images = ['main01.jpg', 'main02.jpg', 'main03.jpg', 'main04.jpg', 'main05.jpg', 'main06.jpg',
+              'main07.jpg', 'main08.jpg', 'main09.jpg', 'main10.jpg', 'main00.jpg'
+            ];
+var NUM_OF_IMAGES = images.length;
+currentImage = 0;
+
+var getImageName = function(id) {
+  return 'images/' + images[id];
+}
+
+var galleryPreload = function() {
+  var image, i;
+  for (i=1; i<NUM_OF_IMAGES; i++) {
+    image = new Image();
+    image.onload = imageReady;
+    image.src = getImageName(i);
+  }
+}
+
+var imageReady = function(e) {
+  console.log('loaded: ' + e.currentTarget.src);
+}
+
+
+var backgroundSwitcher = function () {
+  setInterval(function() {
+    currentImage = (currentImage + 1) % NUM_OF_IMAGES;
+    // $('#image-header').fadeTo('slow', 0.3, function(){
+    //   $(this).css('background-image', 'url(' + getImageName(currentImage) + ')');
+    // }).fadeTo('slow', 1);
+
+    var imageHeader = document.getElementById('image-header');
+    imageHeader.style.backgroundImage = 'url(' + getImageName(currentImage) + ')';
+
+  }, 3 * 1000);
+}
+
+galleryPreload();
+backgroundSwitcher();
--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 80; // Need to set back to 3000!
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public', {
+  extensions: ['html', 'htm'], 
+}));
 
 app.use((req, res, next) => {
   var now = new Date().toString();

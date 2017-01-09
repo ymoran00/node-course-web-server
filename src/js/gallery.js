cdf3b2b8e6778fdf0b71e758b96915cbfc25f7b6
Yoav Moran <yoav.m@taboola.com>
2017-01-09 22:22:08 +0200

    fix gallery behavior

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -4,43 +4,25 @@ var images = ['main00.jpg', 'main01.jpg', 'main02.jpg', 'main03.jpg', 'main04.jp
               'main16.jpg', 'main17.jpg', 'main18.jpg', 'main19.jpg', 'main20.jpg', 'main21.jpg'
             ];
 var NUM_OF_IMAGES = images.length;
-var currentImage = 0;
+var currentImage = Math.floor(Math.random() * NUM_OF_IMAGES);
+var nextImage;
 
 var getImageName = function(id) {
   return 'images/large/' + images[id];
 }
 
-var galleryPreload = function() {
-  var image, i;
-  for (i=1; i<NUM_OF_IMAGES; i++) {
-    image = new Image();
-    console.log('Setting image ' + getImageName(i) + ' to load')
-    image.onload = imageReady;
-    image.src = getImageName(i);
-  }
+
+updateImage = function() {
+  // update the image
+  var imageHeader = document.getElementById('image-header');
+  imageHeader.style.backgroundImage = 'url(' + getImageName(currentImage) + ')';
+  // Preloads next image
+  currentImage = (currentImage + 1) % NUM_OF_IMAGES;
+  var image = new Image();
+   console.log('Setting image ' + getImageName(currentImage) + ' to load')
+  //  image.onload = imageReady;
+   image.src = getImageName(currentImage);
+  // Timeout for switch
+  setTimeout(updateImage, 5 * 1000);
 }
-
-var imageReady = function(e) {
-  console.log('loaded: ' + e.currentTarget.src);
-}
-
-
-var backgroundSwitcher = function () {
-  setInterval(function() {
-    var newId = currentImage;
-    while (newId == currentImage) {
-      var newId = Math.floor(Math.random() * NUM_OF_IMAGES);
-    }
-    currentImage = newId;
-    // $('#image-header').fadeTo('slow', 0.3, function(){
-    //   $(this).css('background-image', 'url(' + getImageName(currentImage) + ')');
-    // }).fadeTo('slow', 1);
-
-    var imageHeader = document.getElementById('image-header');
-    imageHeader.style.backgroundImage = 'url(' + getImageName(currentImage) + ')';
-
-  }, 5 * 1000);
-}
-
-//window.onload(galleryPreload);
-backgroundSwitcher();
+updateImage();
